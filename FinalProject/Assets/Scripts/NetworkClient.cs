37ad98bad818f06ae68b09b13384252057a1a78d
inparia <inparia@live.ca>
2020-10-16 00:29:36 -0400

    Initial project files
    Initial project files

---
+++ FinalProject/Assets/Scripts/NetworkClient.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using Unity.Collections;
+using Unity.Networking.Transport;
+using NetworkMessages;
+using NetworkObjects;
+using System;
+using System.Text;
+
+public class NetworkClient : MonoBehaviour
+{
+    public NetworkDriver m_Driver;
+    public NetworkConnection m_Connection;
+    public string serverIP;
+    public ushort serverPort;
+    private GameObject t;
+    private bool sendInfo;
+    void Start ()
+    {
+        m_Driver = NetworkDriver.Create();
+        m_Connection = default(NetworkConnection);
+        var endpoint = NetworkEndPoint.Parse(serverIP,serverPort);
+        m_Connection = m_Driver.Connect(endpoint);
+        t = GameObject.Find("Cube");
+        sendInfo = false;
+    }
+    
+    void SendToServer(string message){
+        var writer = m_Driver.BeginSend(m_Connection);
+        NativeArray<byte> bytes = new NativeArray<byte>(Encoding.ASCII.GetBytes(message),Allocator.Temp);
+        writer.WriteBytes(bytes);
+        m_Driver.EndSend(writer);
+    }
+
+    void OnConnect(){
+        Debug.Log("We are now connected to the server");
+        sendInfo = true;
+        //// Example to send a handshake message:
+        // HandshakeMsg m = new HandshakeMsg();
+        // m.player.id = m_Connection.InternalId.ToString();
+        // SendToServer(JsonUtility.ToJson(m));
+    }
+
+    void OnData(DataStreamReader stream){
+        NativeArray<byte> bytes = new NativeArray<byte>(stream.Length,Allocator.Temp);
+        stream.ReadBytes(bytes);
+        string recMsg = Encoding.ASCII.GetString(bytes.ToArray());
+        NetworkHeader header = JsonUtility.FromJson<NetworkHeader>(recMsg);
+
+        switch(header.cmd){
+            case Commands.HANDSHAKE:
+            HandshakeMsg hsMsg = JsonUtility.FromJson<HandshakeMsg>(recMsg);
+            Debug.Log("Handshake message received!");
+            break;
+            case Commands.PLAYER_UPDATE:
+            PlayerUpdateMsg puMsg = JsonUtility.FromJson<PlayerUpdateMsg>(recMsg);
+            Debug.Log("Player update message received!");
+            break;
+            case Commands.SERVER_UPDATE:
+            ServerUpdateMsg suMsg = JsonUtility.FromJson<ServerUpdateMsg>(recMsg);
+            Debug.Log("Server update message received!");
+            break;
+            default:
+            Debug.Log("Unrecognized message received!");
+            break;
+        }
+    }
+
+    void Disconnect(){
+        m_Connection.Disconnect(m_Driver);
+        m_Connection = default(NetworkConnection);
+    }
+
+    void OnDisconnect(){
+        Debug.Log("Client got disconnected from server");
+        m_Connection = default(NetworkConnection);
+        Destroy(t);
+        sendInfo = false;
+    }
+
+    public void OnDestroy()
+    {
+        m_Driver.Dispose();
+    }
+    void Update()
+    {
+
+        m_Driver.ScheduleUpdate().Complete();
+
+        if (!m_Connection.IsCreated)
+        {
+            return;
+        }
+
+        DataStreamReader stream;
+        NetworkEvent.Type cmd;
+        cmd = m_Connection.PopEvent(m_Driver, out stream);
+        while (cmd != NetworkEvent.Type.Empty)
+        {
+            if (cmd == NetworkEvent.Type.Connect)
+            {
+                OnConnect();
+
+            }
+            else if (cmd == NetworkEvent.Type.Data)
+            {
+                OnData(stream);
+            }
+            else if (cmd == NetworkEvent.Type.Disconnect)
+            {
+                OnDisconnect();
+            }
+
+            cmd = m_Connection.PopEvent(m_Driver, out stream);
+
+        }
+
+        if (sendInfo) {
+            Debug.Log(t.gameObject.transform.position);
+            SendToServer(JsonUtility.ToJson(t.gameObject.transform.position));
+        }
+    }
+}
---
+++ FinalProject/Assets/Scripts/NetworkServer.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using Unity.Collections;
+using Unity.Networking.Transport;
+using NetworkMessages;
+using System;
+using System.Text;
+
+public class NetworkServer : MonoBehaviour
+{
+    public NetworkDriver m_Driver;
+    public ushort serverPort;
+    private NativeList<NetworkConnection> m_Connections;
+
+    void Start ()
+    {
+        m_Driver = NetworkDriver.Create();
+        var endpoint = NetworkEndPoint.AnyIpv4;
+        endpoint.Port = serverPort;
+        if (m_Driver.Bind(endpoint) != 0)
+            Debug.Log("Failed to bind to port " + serverPort);
+        else
+            m_Driver.Listen();
+
+        m_Connections = new NativeList<NetworkConnection>(16, Allocator.Persistent);
+    }
+
+    void SendToClient(string message, NetworkConnection c){
+        var writer = m_Driver.BeginSend(NetworkPipeline.Null, c);
+        NativeArray<byte> bytes = new NativeArray<byte>(Encoding.ASCII.GetBytes(message),Allocator.Temp);
+        writer.WriteBytes(bytes);
+        m_Driver.EndSend(writer);
+    }
+    public void OnDestroy()
+    {
+        m_Driver.Dispose();
+        m_Connections.Dispose();
+    }
+
+    void OnConnect(NetworkConnection c){
+        m_Connections.Add(c);
+        Debug.Log("Accepted a connection");
+
+        //// Example to send a handshake message:
+        // HandshakeMsg m = new HandshakeMsg();
+        // m.player.id = c.InternalId.ToString();
+        // SendToClient(JsonUtility.ToJson(m),c);        
+    }
+
+    void OnData(DataStreamReader stream, int i){
+        NativeArray<byte> bytes = new NativeArray<byte>(stream.Length,Allocator.Temp);
+        stream.ReadBytes(bytes);
+        string recMsg = Encoding.ASCII.GetString(bytes.ToArray());
+        NetworkHeader header = JsonUtility.FromJson<NetworkHeader>(recMsg);
+
+        switch(header.cmd){
+            case Commands.HANDSHAKE:
+            HandshakeMsg hsMsg = JsonUtility.FromJson<HandshakeMsg>(recMsg);
+            Debug.Log("Handshake message received!");
+            break;
+            case Commands.PLAYER_UPDATE:
+            PlayerUpdateMsg puMsg = JsonUtility.FromJson<PlayerUpdateMsg>(recMsg);
+            //Debug.Log("Player update message received!");
+            Debug.Log(recMsg);
+            break;
+            case Commands.SERVER_UPDATE:
+            ServerUpdateMsg suMsg = JsonUtility.FromJson<ServerUpdateMsg>(recMsg);
+            Debug.Log("Server update message received!");
+            break;
+            default:
+            Debug.Log("SERVER ERROR: Unrecognized message received!");
+            break;
+        }
+    }
+
+    void OnDisconnect(int i){
+        Debug.Log("Client disconnected from server");
+        m_Connections[i] = default(NetworkConnection);
+    }
+
+    void Update ()
+    {
+        m_Driver.ScheduleUpdate().Complete();
+
+        // CleanUpConnections
+        for (int i = 0; i < m_Connections.Length; i++)
+        {
+            if (!m_Connections[i].IsCreated)
+            {
+
+                m_Connections.RemoveAtSwapBack(i);
+                --i;
+            }
+        }
+
+        // AcceptNewConnections
+        NetworkConnection c = m_Driver.Accept();
+        while (c  != default(NetworkConnection))
+        {            
+            OnConnect(c);
+
+            // Check if there is another new connection
+            c = m_Driver.Accept();
+        }
+
+
+        // Read Incoming Messages
+        DataStreamReader stream;
+        for (int i = 0; i < m_Connections.Length; i++)
+        {
+            Assert.IsTrue(m_Connections[i].IsCreated);
+            
+            NetworkEvent.Type cmd;
+            cmd = m_Driver.PopEventForConnection(m_Connections[i], out stream);
+            while (cmd != NetworkEvent.Type.Empty)
+            {
+                if (cmd == NetworkEvent.Type.Data)
+                {
+                    OnData(stream, i);
+                }
+                else if (cmd == NetworkEvent.Type.Disconnect)
+                {
+                    OnDisconnect(i);
+                }
+
+                cmd = m_Driver.PopEventForConnection(m_Connections[i], out stream);
+            }
+
+            
+        }
+       
+
+    }
+}
