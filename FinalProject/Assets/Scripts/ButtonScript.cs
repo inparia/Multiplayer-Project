e53224994d089f7466c0fb1cb58a1379f80cc2b8
inparia <inparia@live.ca>
2020-10-19 00:18:59 -0400

    Minor Changes

--- FinalProject/Assets/Scripts/ButtonScript.cs
+++ FinalProject/Assets/Scripts/ButtonScript.cs
@@ -5,10 +5,16 @@ using UnityEngine.SceneManagement;
 public class ButtonScript : MonoBehaviour
 {
     public GameManager gameManager;
-    public void ButtonNext(string str)
+
+    public void ButtonNext()
+    {
+         SceneManager.LoadScene("ClientScene");
+    }
+
+    public void StopButton()
     {
         //set the name of the next scene by the string input
-        if(gameManager.sum <= gameManager.GetGameNumber() + 3 && gameManager.sum >= gameManager.GetGameNumber() - 3)
+        if (gameManager.sum <= gameManager.GetGameNumber() + 3 && gameManager.sum >= gameManager.GetGameNumber() - 3)
         {
             SceneManager.LoadScene("WinScene");
         }
